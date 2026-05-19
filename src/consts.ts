40d65d2b5e37f7a5a544f1fed664a387a71dbec7
yuhaixin0924 <1528259132@qq.com>
2026-05-19 09:27:53 +0800

    chore: initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '我的学习博客';
+export const SITE_DESCRIPTION = '记录建站、前端和学习过程的个人博客。';
